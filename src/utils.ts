7821abda1c24fed8a418f2f608b0d943b373c11c
Devanshu Sharma <devanshusharma658@gmail.com>
2025-01-17 16:23:06 +0530

    init

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx"
+import { Hexagon } from "lucide-react"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const parseColor = (color: string) => {
+  const hex = color.startsWith("#") ? color.slice(1) : color
+  return parseInt(hex,16)
 }
